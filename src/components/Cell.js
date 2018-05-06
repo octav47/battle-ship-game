736c19b30d8a6a0153e5d21cc01db5b6f17c59ea
octav47 <forty-7@yandex.ru>
2018-05-06 08:22:25 +0300

    added styles

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const Cell = props => {
-    return (
-        <div>
-            i'm cell
-        </div>
-    )
+import styles from './styles/Cell.sass'
+
+class Cell extends React.Component {
+    render () {
+        const { x, y } = this.props
+
+        return (
+            <div className={styles.cell}>
+                {x},{y}
+            </div>
+        )
+    }
 }
 
 export default Cell
