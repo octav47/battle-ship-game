4a4b22471ae9e6c8ed7aa68df616a76b6f63d668
Cyrus <forty-7@yandex.ru>
2018-05-27 15:43:31 +0300

    eslint

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import gameReducer from './gameReducer'
+import gameReducer from 'Reducers/gameReducer'
 
 const rootReducer = combineReducers({
     game: gameReducer,
